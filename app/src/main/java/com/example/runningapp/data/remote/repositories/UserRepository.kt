7151918a97268bf8a70cf359111a5499c81b7480
DATTRAN2207 <datvu2207@gmail.com>
2023-04-19 14:15:41 +0700

    add signup usecase

--- app/src/main/java/com/example/runningapp/data/remote/repositories/UserRepository.kt
+++ app/src/main/java/com/example/runningapp/data/remote/repositories/UserRepository.kt
@@ -4,7 +4,15 @@ import com.example.runningapp.data.remote.dto.ApiResponse
 import com.example.runningapp.data.remote.dto.user.*
 
 interface UserRepository {
+<<<<<<< HEAD
     suspend fun login(body: LoginRequestDTO) : ApiResponse<LoginResponseDataDTO>?
+<<<<<<< HEAD
     suspend fun signup(body: SignupRequestDTO) : ApiResponse<SignupResponseDataDTO>?
 
+=======
+=======
+    suspend fun login(body: LoginRequest) : ApiResponse<LoginResponseData>?
+    suspend fun signup(body: SignupRequest) : ApiResponse<SignupResponseData>?
+>>>>>>> 0a4ed7c (add signup usecase)
+>>>>>>> 14787a1 (add signup usecase)
 }
