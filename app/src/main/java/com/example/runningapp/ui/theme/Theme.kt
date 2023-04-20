b2a91289dbad8a006743929e7d349b73cb2fd95b
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-21 01:26:01 +0700

    refactor(Theme): replace deprecated function

--- app/src/main/java/com/example/runningapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/runningapp/ui/theme/Theme.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -57,7 +58,7 @@ fun RunningAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController((view.context as Activity).window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
