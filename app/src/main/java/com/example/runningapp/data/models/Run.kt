babe4fd52875b523ac7423a1cb44f6d0a9f68411
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-10 03:54:20 +0700

    feat(data): add Run, Round, LatLng realm models

---
+++ app/src/main/java/com/example/runningapp/data/models/Run.kt
@@ -0,0 +1,25 @@
+package com.example.runningapp.data.models
+
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.EmbeddedRealmObject
+import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class Run : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    var rounds: RealmList<Round> = realmListOf()
+
+    class Round : EmbeddedRealmObject {
+        var points: RealmList<LatLng> = realmListOf()
+        var meters: Double = 0.0
+        var seconds: Long = 0
+
+        class LatLng : EmbeddedRealmObject {
+            var lat: Double = 0.0
+            var lng: Double = 0.0
+        }
+    }
+}
