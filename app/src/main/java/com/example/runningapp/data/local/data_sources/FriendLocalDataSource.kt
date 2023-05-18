a68dfc3812c1c8bb80edae450de9fc8bcb5b3199
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-19 04:25:14 +0700

    refactor: organize imports

--- app/src/main/java/com/example/runningapp/data/local/data_sources/FriendLocalDataSource.kt
+++ app/src/main/java/com/example/runningapp/data/local/data_sources/FriendLocalDataSource.kt
@@ -1,15 +1,10 @@
 package com.example.runningapp.data.local.data_sources
 
 import com.example.runningapp.data.models.Friend
-import com.example.runningapp.data.models.Run
-import com.example.runningapp.data.models.User
 import io.realm.kotlin.Realm
 import io.realm.kotlin.ext.query
-import io.realm.kotlin.ext.toRealmList
-import io.realm.kotlin.notifications.SingleQueryChange
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 import javax.inject.Singleton
 
